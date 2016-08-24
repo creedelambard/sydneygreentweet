869c7f8c73621b3614481b913929b4baea0e6876
Creede Lambard <creede@lambard.net>
2016-08-23 22:52:30 -0700

    Authentication is working; need to rework index page, store twitter tokens in DB

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     auth = env["omniauth.auth"]
-    #Rails.logger.info("auth is **************** #{auth.to_yaml}")
+    # Rails.logger.info("auth is **************** #{auth.to_yaml}")
+    puts "New user registration URL is #{new_user_registration_url}"
     @user = User.find_for_twitter_oauth(request.env["omniauth.auth"],current_user)
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
+      # that_banjo_guy is persisted
+      flash[:notice] = "Welcome to Sydney!"
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.twitter_uid"] = request.env["omniauth.auth"]
